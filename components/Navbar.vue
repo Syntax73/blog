c28a625620520b490b54313198cc146a1596cb52
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2022-10-31 07:58:15 -0300

    Movido estilo para css global

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,35 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
+
+const asdf = () => {
+	console.log('sdf');
+};
+
+[].map(e => console.log(e));
 
 const links = ref([
-  {
-    title: "Postagens",
-    path: "/postagens",
-  },
-  {
-    title: "Sobre",
-    path: "/about",
-  },
+	{
+		title: 'Postagens',
+		path: '/postagens',
+	},
+	{
+		title: 'Sobre',
+		path: '/about',
+	},
 ]);
 </script>
 
 <template>
-  <nav class="p-4 bg-neutral-600 flex justify-around">
-    <nuxt-link to="/">LOGO</nuxt-link>
+	<nav class="p-4 bg-neutral-600 flex justify-around">
+		<nuxt-link to="/">LOGO</nuxt-link>
 
-    <ul class="flex justify-between w-60">
-      <li v-for="(link, index) in links" :key="index" class="">
-        <nuxt-link :to="link.path">
-          {{ link.title }}
-        </nuxt-link>
-      </li>
-      <button>Dark Mode</button>
-    </ul>
-  </nav>
+		<ul class="flex justify-between w-60">
+			<li v-for="(link, index) in links" :key="index" class="">
+				<nuxt-link :to="link.path">
+					{{ link.title }}
+				</nuxt-link>
+			</li>
+			<button>Dark Mode</button>
+		</ul>
+	</nav>
 </template>
--- pages/postagens/[...slug].vue
+++ pages/postagens/[...slug].vue
@@ -2,66 +2,20 @@
 const route = useRoute();
 
 const { data } = await useAsyncData(`content-${route.path}`, () =>
-  queryContent().where({ _path: route.path }).findOne()
+	queryContent().where({ _path: route.path }).findOne()
 );
 </script>
 
 <template>
-  <main class="max-w-4xl m-auto p-3">
-    <BlogHeader
-      :title="data.title"
-      :author="data.author"
-      :description="data.description"
-      :tags="data.tags"
-      :created-at="data.createdAt"
-    />
+	<main class="max-w-4xl m-auto p-3">
+		<BlogHeader
+			:title="data.title"
+			:author="data.author"
+			:description="data.description"
+			:tags="data.tags"
+			:created-at="data.createdAt"
+		/>
 
-    <ContentRenderer :value="data" class="content" />
-  </main>
+		<ContentRenderer :value="data" class="content" />
+	</main>
 </template>
-
-<style scoped>
-.content {
-  @apply text-gray-900;
-}
-
-.content :deep(h1) {
-  @apply text-3xl font-bold mb-3;
-}
-
-.content :deep(h2) {
-  @apply text-2xl font-semibold mb-3;
-}
-
-.content :deep(h3) {
-  @apply text-xl font-semibold mb-3;
-}
-
-.content :deep(h4) {
-  @apply text-lg font-semibold mb-3;
-}
-
-.content :deep(h5) {
-  @apply text-base font-semibold mb-3;
-}
-
-.content :deep(h6) {
-  @apply text-base font-semibold mb-3;
-}
-
-.content :deep(p) {
-  @apply mb-3;
-}
-
-.content :deep(ol) {
-  @apply list-decimal list-inside pl-4;
-}
-
-.content :deep(ul) {
-  @apply list-disc list-inside pl-4;
-}
-
-.content :deep(blockquote) {
-  @apply border-l-2 border-gray-700 pl-3;
-}
-</style>
