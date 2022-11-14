83a126487c4fb8537c773a89c432bb59756e1863
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2022-11-13 22:17:56 -0300

    Criado input de pesquisa e accordion de postagens

--- pages/postagens/index.vue
+++ pages/postagens/index.vue
@@ -5,16 +5,50 @@ const { data: articles } = await useAsyncData('posts', () =>
 </script>
 
 <template>
-	<main class="mt-2 max-w-5xl m-auto p-3 gap-6 grid">
-		<ArticleCard
-			v-for="post in articles"
-			:key="post._id"
-			:title="post.title"
-			:description="post.description"
-			:preview="post.preview"
-			:author="post.author"
-			:path="post._path"
-			:reading-time="12"
-		/>
+	<main class="mt-2 max-w-5xl m-auto p-3">
+		<article class="grid grid-cols-6 gap-3">
+			<section class="col-span-4 gap-6 flex flex-col">
+				<ArticleCard
+					v-for="post in articles"
+					:key="post._id"
+					:title="post.title"
+					:description="post.description"
+					:preview="post.preview"
+					:author="post.author"
+					:path="post._path"
+					:reading-time="12"
+				/>
+			</section>
+			<aside class="col-span-2 flex flex-col gap-3">
+				<div class="bg-light-300 dark:bg-slate-700 rounded-md p-3 w-full">
+					<div class="relative flex items-center rounded-md overflow-hidden">
+						<input
+							type="text"
+							class="bg-slate-800 outline-none pl-1 py-2 w-full"
+						/>
+						<button class="bg-slate-900 py-1 px-2 rounded-md absolute right-1">
+							Pesquisar
+						</button>
+					</div>
+				</div>
+
+				<div class="bg-light-300 dark:bg-slate-700 rounded-md p-3 w-full">
+					<details>
+						<summary>2022 (1)</summary>
+						<details class="ml-2">
+							<summary>novembro</summary>
+							<span class="ml-5">Body Content 1</span>
+						</details>
+					</details>
+					<details>
+						<summary>2021 (1)</summary>
+						<details class="ml-2">
+							<summary>novembro</summary>
+							<span class="ml-5">Body Content 1</span>
+						</details>
+					</details>
+				</div>
+			</aside>
+		</article>
 	</main>
 </template>
